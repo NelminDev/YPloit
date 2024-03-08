2c55c940cc2c5022c1be10f57dddc7b8e3ae1a82
Nelmin <nelmindev@proton.me>
2024-03-08 18:44:10 +0100

    Removed Embed URL

--- src/main/kotlin/me/nelmin/minecraft/utils/discord/DiscordWebhook.kt
+++ src/main/kotlin/me/nelmin/minecraft/utils/discord/DiscordWebhook.kt
@@ -1,21 +1,20 @@
-package me.nelmin.minecraft.utils
+package me.nelmin.minecraft.utils.discord
 
+import me.nelmin.minecraft.Secrets
 import java.io.IOException
 import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
 
-object DiscordWebhookSender {
-
-    private const val DISCORD_WEBHOOK_URL = "https://discord.com/api/webhooks/1215694499842363424/WhwB_JfpHA-iZ-4L_w6hA2flg-YvFoJDsx4NGGslPoD_VARdW6XTFZZs1zwB4p7nnDE_"
+class DiscordWebhookSender {
 
     @Throws(IOException::class, InterruptedException::class)
     fun sendMessage(message: String) {
         val client = HttpClient.newHttpClient()
 
         val request = HttpRequest.newBuilder()
-            .uri(URI.create(DISCORD_WEBHOOK_URL))
+            .uri(URI.create(Secrets.DISCORD_WEBHOOK_URL))
             .header("Content-Type", "application/json")
             .POST(HttpRequest.BodyPublishers.ofString("{\"content\": \"$message\"}"))
             .build()
